6aeb0643b4d697712b9a8c475254322d739b288f
Serhii Vostrikov <65418387+Antiserggio@users.noreply.github.com>
2023-11-09 21:56:37 +0200

    Summary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'App';
 import './index.css';
+import data from './data.json';
+import GetArrElem from './GetArrElem';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
